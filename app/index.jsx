3051b6ff15decb4a99e86660a4bcd0eba790a55f
Sergey <seregaland@yandex.ru>
2024-10-23 14:49:56 +0300

    first commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,19 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, Link } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import {Text, View} from 'react-native';
+import "../global.css";// Import your global CSS file
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { StatusBar } from 'expo-status-bar';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default function App() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -28,10 +31,25 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      {/* <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
-      </Stack>
+      </Stack> */}
+      <View className="flex-1 items-center justify-center bg-white">
+       <Text className="text-3xl text-center">Майкопский молодежный сборник</Text>
+       <StatusBar style='auto' />
+       <Link href="/profile" style={{color: 'blue'}}>Перейти в Профиль</Link>
+      </View>
     </ThemeProvider>
   );
 }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     }
+//   })
+
