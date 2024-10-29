679740e866eb2149beda2a3dcd093ad2944ae547
Sergey <seregaland@yandex.ru>
2024-10-29 17:08:54 +0300

    fix icon

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -4,34 +4,110 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { icons } from "../../constants";
 
-export default function TabLayout() {
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+const TabLayout = () => {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
+        tabBarActiveTintColor: "#FFA001",
+        tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#161622",
+          borderTopWidth: 1,
+          borderTopColor: "#232533",
+          height: 84,
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Главная',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon 
+              icon={icons.home}
+              name={focused ? 'code-slash' : 'code-slash-outline'} 
+              color={color} 
+            />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="songs"
         options={{
-          title: 'Explore',
+          title: 'Песни',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon 
+              icon={icons.profile}
+              name={focused ? 'code-slash' : 'code-slash-outline'} 
+              color={color} 
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="playlists"
+        options={{
+          title: 'Плейлисты',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon 
+              icon={icons.profile}
+              name={focused ? 'code-slash' : 'code-slash-outline'} 
+              color={color} 
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorite"
+        options={{
+          title: 'Избранное',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon 
+              icon={icons.bookmark}
+              name={focused ? 'code-slash' : 'code-slash-outline'} 
+              color={color} 
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notes"
+        options={{
+          title: 'Заметки',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon 
+              icon={icons.bookmark}
+              name={focused ? 'code-slash' : 'code-slash-outline'} 
+              color={color} 
+            />
           ),
         }}
       />
     </Tabs>
   );
 }
+
+export default TabLayout;
---
+++ app/(tabs)/playlists.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const playlists = () => {
+  return (
+    <View>
+      <Text>playlists</Text>
+    </View>
+  )
+}
+
+export default playlists
+
+const styles = StyleSheet.create({})
