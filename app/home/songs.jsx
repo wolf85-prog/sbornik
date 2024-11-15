f0c3e0467c553add3c7f358ec7636c12dbed602b
Sergey <seregaland@yandex.ru>
2024-11-15 17:08:35 +0300

    fix sqlite

--- app/home/songs.jsx
+++ app/home/songs.jsx
@@ -1,14 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button, Platform  } from 'react-native'
 import React from 'react'
 
+import * as SQLite from 'expo-sqlite';
+import { useState, useEffect } from 'react';
+import * as Sharing from 'expo-sharing';
+import * as FileSystem from 'expo-file-system';
+import * as DocumentPicker from 'expo-document-picker';
+
 const songs = () => {
+  const [db, setDb] = useState(SQLite.openDatabaseAsync('example.db'));
+  const [isLoading, setIsLoading] = useState(true);
+  const [names, setNames] = useState([]);
+  const [currentName, setCurrentName] = useState(undefined);
+
+
+  useEffect(() => {
+    // db.transaction(tx => {
+    //   tx.executeSql('CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
+    // });
+
+    db.execAsync(tx => {
+      tx.executeSql('CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
+    });
+
+
+    setIsLoading(false);
+  }, [db]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading names...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>songs</Text>
+    <View style={styles.container}>
+      <TextInput value={currentName} placeholder='name' onChangeText={setCurrentName} />
+      <Button title="Add Name" onPress={addName} />
+      {/* {showNames()} */}
+      {/* <Button title="Export Db" onPress={exportDb} />
+      <Button title="Import Db" onPress={importDb} /> */}
+      <StatusBar style="auto" />
     </View>
   )
 }
 
 export default songs
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    margin: 8
+  }
+});
